2df42a41c93bd23caea6816f784372385322d4a0
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2021-09-23 15:51:19 -0700

    Test headers too

--- src/main/java/envoy/RandomHolder.java
+++ src/main/java/envoy/RandomHolder.java
@@ -6,6 +6,16 @@ public class RandomHolder {
 
     public static Random RANDOM = new Random();
 
+    public static byte[] bytes(final int sz) {
+        final byte[] bytes = new byte[sz];
+        RANDOM.nextBytes(bytes);
+        return bytes;
+    }
+
+    public static int nextInt(final int bound) {
+        return RANDOM.nextInt(bound);
+    }
+
     private RandomHolder() {
     }
 
--- src/main/java/envoy/Records.java
+++ src/main/java/envoy/Records.java
@@ -1,32 +1,53 @@
 package envoy;
 
 import java.util.Arrays;
-import java.util.Random;
+import java.util.List;
+import java.util.Objects;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 public class Records {
 
-    private static final Random RANDOM = new Random();
-
     public static ProducerRecord<byte[], byte[]> makeRecord(final String topic) {
-        final byte[] key = new byte[256];
-        RANDOM.nextBytes(key);
-        final byte[] value = new byte[2048];
-        RANDOM.nextBytes(value);
-        return new ProducerRecord<>(topic, key, value);
+        final byte[] key = RandomHolder.bytes(256);
+        final byte[] value = RandomHolder.bytes(2048);
+        final ProducerRecord<byte[], byte[]> record = new ProducerRecord<>(topic, key, value);
+        for (int i = 0; i < RandomHolder.nextInt(32); ++i) {
+            for (int j = 0; j < RandomHolder.nextInt(3); j++) {
+                // Let's make some records with the same names.
+                record.headers().add("hh_" + i, RandomHolder.bytes(128));
+            }
+        }
+        return record;
     }
 
     /**
      * This method needs to be aware of what Envoy does with records.
-     * Right now no headers!
      */
     public static boolean equalRecordContents(final ProducerRecord<byte[], byte[]> sentRecord,
                                               final ConsumerRecord<byte[], byte[]> receivedRecord) {
 
-        return Arrays.equals(sentRecord.key(), receivedRecord.key())
+        final boolean equalData = Arrays.equals(sentRecord.key(), receivedRecord.key())
                 && Arrays.equals(sentRecord.value(), receivedRecord.value());
+        if (!equalData) {
+            return false;
+        }
+
+        final boolean equalHeaderCounts = Iterables.size(sentRecord.headers()) == Iterables
+                .size(receivedRecord.headers());
+        if (!equalHeaderCounts) {
+            return false;
+        }
+
+        final List<Header> sentHeaders = Lists.newArrayList(sentRecord.headers());
+        final List<Header> receivedHeaders = Lists.newArrayList(receivedRecord.headers());
+        // RecordHeader.equals appears reasonable
+        return Objects.equals(sentHeaders, receivedHeaders);
     }
 
     private Records() {
